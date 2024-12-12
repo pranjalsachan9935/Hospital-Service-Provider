8a1accc6cd354737c06390db35cd0f924c90e50b
Pranjal Sachan <146807790+pranjalsachan9935@users.noreply.github.com>
2024-12-12 16:22:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./component/Layout";
 import Cart from "./page/Cart";
@@ -7,28 +7,32 @@ import FAQ from "./page/FAQ";
 import Claim from "./page/Claim";
 import SlidetoTop from "./component/SlidetoTop";
 import Sidebar from "./component/Sidebar";
+import Loading from "./component/Loading";
 const App = () => {
 
+  
   function handleTop() {
     window.scrollTo({
       top: 0,
-      behavior: "smooth", 
+      behavior: "smooth",
     });
   }
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout handleTop={handleTop}/>} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/claim" element={<Claim />} />
-        </Routes>
-      </BrowserRouter>
-      <Sidebar />
-      <SlidetoTop handleTop={handleTop}/>
+      <Suspense fallback={<Loading/>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout handleTop={handleTop} />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/claim" element={<Claim />} />
+          </Routes>
+          <Sidebar />
+          <SlidetoTop handleTop={handleTop} />
+        </BrowserRouter>
+      </Suspense>
     </>
   );
 };
